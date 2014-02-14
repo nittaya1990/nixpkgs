ab4245cab216efe0c361967e1da22b8bc1ba74a5
Corey O'Connor <coreyoconnor@gmail.com>
2014-02-13 16:49:16 -0800

    allow extension of the jenkins service environment

--- nixos/modules/services/continuous-integration/jenkins/default.nix
+++ nixos/modules/services/continuous-integration/jenkins/default.nix
@@ -47,13 +47,16 @@ in {
       packages = mkOption {
         default = [ pkgs.stdenv pkgs.git pkgs.jdk pkgs.openssh pkgs.nix ];
         type = types.listOf types.package;
-        description = "The packages to expose to the jenkins process";
+        description = "Packages to expose to the jenkins process";
       };
 
-      nixRemote = mkOption {
-        default = "daemon";
-        type = types.string;
-        description = "The NIX_REMOTE variable to provide to the jenkins process";
+      environment = mkOption {
+        default = { NIX_REMOTE = "daemon"; };
+        type = with types; attrsOf string;
+        description = ''
+          Additional environment variables to be passed to the jenkins process.
+          The environment will always include JENKINS_HOME.
+        '';
       };
     };
   };
@@ -78,12 +81,11 @@ in {
 
       environment = {
         JENKINS_HOME = cfg.home;
-      };
+      } // cfg.environment;
 
       path = cfg.packages;
 
       script = ''
-        export NIX_REMOTE=${cfg.nixRemote}
         ${pkgs.jdk}/bin/java -jar ${pkgs.jenkins} --httpPort=${toString cfg.port}
       '';
 
