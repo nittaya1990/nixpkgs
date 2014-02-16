ad6ca75426db91c30c6bffe20277e88ad252076e
Corey O'Connor <coreyoconnor@gmail.com>
2014-02-15 18:33:00 -0800

    add the node definition to an import in build-vms instead of adding it to the list of modules.

--- nixos/lib/build-vms.nix
+++ nixos/lib/build-vms.nix
@@ -19,12 +19,13 @@ rec {
 
 
   buildVM =
-    nodes: configurations:
+    nodes: configuration:
 
     import ./eval-config.nix {
       inherit system;
-      modules = configurations ++
-        [ ../modules/virtualisation/qemu-vm.nix
+      modules =
+        [ configuration
+          ../modules/virtualisation/qemu-vm.nix
           ../modules/testing/test-instrumentation.nix # !!! should only get added for automated test runs
           { key = "no-manual"; services.nixosManual.enable = false; }
         ] ++ optional minimal ../modules/testing/minimal-kernel.nix;
@@ -35,53 +36,50 @@ rec {
   # Given an attribute set { machine1 = config1; ... machineN =
   # configN; }, sequentially assign IP addresses in the 192.168.1.0/24
   # range to each machine, and set the hostname to the attribute name.
-  assignIPAddresses = nodes:
+  assignIPAddresses = nodes_def:
 
     let
 
-      machines = attrNames nodes;
+      machines = attrNames nodes_def;
 
       machinesNumbered = zipTwoLists machines (range 1 254);
 
       nodes_ = flip map machinesNumbered (m: nameValuePair m.first
-        [ ( { config, pkgs, nodes, ... }:
-            let
-              interfacesNumbered = zipTwoLists config.virtualisation.vlans (range 1 255);
-              interfaces = flip map interfacesNumbered ({ first, second }:
-                nameValuePair "eth${toString second}"
-                  { ipAddress = "192.168.${toString first}.${toString m.second}";
-                    subnetMask = "255.255.255.0";
-                  });
-            in
-            { key = "ip-address";
-              config =
-                { networking.hostName = m.first;
+        ({ config, pkgs, nodes, ... }:
+        let
+          interfacesNumbered = zipTwoLists config.virtualisation.vlans (range 1 255);
+          interfaces = flip map interfacesNumbered ({ first, second }:
+            nameValuePair "eth${toString second}"
+              { ipAddress = "192.168.${toString first}.${toString m.second}";
+                subnetMask = "255.255.255.0";
+              });
+        in
+        { key = "ip-address";
+          imports = [ (getAttr m.first nodes_def) ];
+          config =
+            { networking.hostName = mkOverride 900 m.first;
 
-                  networking.interfaces = listToAttrs interfaces;
+              networking.interfaces = listToAttrs interfaces;
 
-                  networking.primaryIPAddress =
-                    optionalString (interfaces != []) (head interfaces).value.ipAddress;
+              networking.primaryIPAddress =
+                optionalString (interfaces != []) (head interfaces).value.ipAddress;
 
-                  # Put the IP addresses of all VMs in this machine's
-                  # /etc/hosts file.  If a machine has multiple
-                  # interfaces, use the IP address corresponding to
-                  # the first interface (i.e. the first network in its
-                  # virtualisation.vlans option).
-                  networking.extraHosts = flip concatMapStrings machines
-                    (m': let config = (getAttr m' nodes).config; in
-                      optionalString (m.first != m' && config.networking.primaryIPAddress != "")
-                        ("${config.networking.primaryIPAddress} " +
-                         "${config.networking.hostName}\n"));
+              # Put the IP addresses of all VMs in this machine's
+              # /etc/hosts file.  If a machine has multiple
+              # interfaces, use the IP address corresponding to
+              # the first interface (i.e. the first network in its
+              # virtualisation.vlans option).
+              networking.extraHosts = flip concatMapStrings machines
+                (m': let config = (getAttr m' nodes).config; in
+                  optionalString (m.first != m' && config.networking.primaryIPAddress != "")
+                    ("${config.networking.primaryIPAddress} " +
+                     "${config.networking.hostName}\n"));
 
-                  virtualisation.qemu.options =
-                    flip map interfacesNumbered
-                      ({ first, second }: qemuNICFlags second first m.second);
-                };
-            }
-          )
-          (getAttr m.first nodes)
-        ] );
+              virtualisation.qemu.options =
+                flip map interfacesNumbered
+                  ({ first, second }: qemuNICFlags second first m.second);
+            };
+        }));
 
     in listToAttrs nodes_;
-
 }
