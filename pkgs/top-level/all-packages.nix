a880fcdf74ae31069c3e0e6f3d5a4e8988db68e5
Thomas Tuegel <ttuegel@gmail.com>
2015-01-19 11:14:14 -0600

    orion: add GTK3 theme

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12372,6 +12372,8 @@ let
     geoclue = geoclue2;
   };
 
+  orion = callPackage ../misc/themes/orion {};
+
   oxygen-gtk2 = callPackage ../misc/themes/gtk2/oxygen-gtk { };
 
   oxygen-gtk3 = callPackage ../misc/themes/gtk3/oxygen-gtk3 { };
