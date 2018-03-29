5e3ce30285a97109ec065ad8f504e36bcf87d887
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-29 16:20:41 -0700

    qgis: add 3.0.1.

---
+++ pkgs/applications/gis/qgis/wrapped.nix
@@ -0,0 +1,46 @@
+{ stdenv, lib, fetchurl, openssl, python3Packages, makeWrapper, symlinkJoin
+, qgis3-unwrapped
+}:
+with lib;
+let
+  # the python packages that should be included in the PYTHONPATH
+  pythonInputs = with python3Packages;
+    qgis3-unwrapped.pythonBuildInputs ++ [ chardet dateutil pyyaml pytz requests urllib3 ];
+in symlinkJoin rec {
+  inherit (qgis3-unwrapped) version;
+  name = "qgis-${version}";
+
+  paths = [ qgis3-unwrapped ];
+
+  nativeBuildInputs = [ makeWrapper python3Packages.wrapPython ];
+
+  inherit pythonInputs;
+
+  # use the source archive directly to avoid rebuilding when changing qgis distro
+  inherit (qgis3-unwrapped) src;
+
+  postBuild = ''
+    unpackPhase
+
+    buildPythonPath "$pythonInputs"
+
+    wrapProgram $out/bin/qgis \
+      --prefix PATH : $program_PATH \
+      --set PYTHONPATH $program_PYTHONPATH
+
+    # desktop link
+    mkdir -p $out/share/applications
+
+    sed "/^Exec=/c\Exec=$out/bin/qgis" \
+      < $sourceRoot/debian/qgis.desktop \
+      > $out/share/applications/qgis.desktop
+
+    # mime types
+    mkdir -p $out/share/mime/packages
+    cp $sourceRoot/debian/qgis.xml $out/share/mime/packages
+
+    # vector icon
+    mkdir -p $out/share/icons/hicolor/scalable/apps
+    cp $sourceRoot/images/icons/qgis_icon.svg $out/share/icons/hicolor/scalable/apps/qgis.svg
+  '';
+}
