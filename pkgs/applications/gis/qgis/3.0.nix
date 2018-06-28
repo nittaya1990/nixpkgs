c27d9fdd1e51bec03d66170b75ced8b5fdde7205
Corey O'Connor <coreyoconnor@gmail.com>
2018-06-27 18:21:29 -0700

    qgis: 3.0.1 -> 3.2

--- pkgs/applications/gis/qgis/3.0.nix
+++ pkgs/applications/gis/qgis/3.0.nix
@@ -10,12 +10,12 @@ let
     [ python3Packages.qscintilla gdal jinja2 numpy psycopg2
       chardet dateutil pyyaml pytz requests urllib3 pygments pyqt5 sip OWSLib six ];
 in stdenv.mkDerivation rec {
-  version = "3.0.1";
+  version = "3.2.0";
   name = "qgis-unwrapped-${version}";
 
   src = fetchurl {
     url = "http://qgis.org/downloads/qgis-${version}.tar.bz2";
-    sha256 = "1m24kjl784csbv0dgx1wbdwg8r92cpc1j718aaw85p7vgicm8acy";
+    sha256 = "1zxylby32d3l4rgi3w7kglx5q2qk0pkhq9ah1wgsqny0vrdwriqb";
   };
 
   inherit pythonBuildInputs;
