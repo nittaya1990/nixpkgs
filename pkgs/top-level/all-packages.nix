18362887c6d7a547c0296fb3236b5adf43b029b3
zimbatm <zimbatm@zimbatm.com>
2018-10-26 01:17:55 +0200

    openssh: 7.7p1 -> 7.9p1 (#48784)
    added openssh_gssapi to make it easier to test the patched version
    
    the HPN edition isn't available on top of 7.9p1 yet
    
    fix-host-key-algorithms-plus.patch didn't apply anymore, assuming it's
    fixed.
    
    release notes: https://www.openssh.com/txt/release-7.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4520,6 +4520,10 @@ with pkgs;
 
   openssh_hpn = pkgs.appendToName "with-hpn" (openssh.override { hpnSupport = true; });
 
+  openssh_gssapi = pkgs.appendToName "with-gssapi" (openssh.override {
+    withGssapiPatches = true;
+  });
+
   opensp = callPackage ../tools/text/sgml/opensp { };
 
   opentracker = callPackage ../applications/networking/p2p/opentracker { };
